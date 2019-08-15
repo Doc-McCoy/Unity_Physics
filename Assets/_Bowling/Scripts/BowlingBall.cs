d6645365d2506f951f7ca66e0c6e65a8a627f56a
Renan <renan_ota@hotmail.com>
2019-08-14 22:31:42 -0300

    Aula 01
    Boliche

---
+++ Assets/_Bowling/Scripts/BowlingBall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowlingBall : MonoBehaviour
+{
+    public float m_Force = 100.0f;
+    private Rigidbody m_Rigidbody;
+
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void OnMouseDown()
+    {
+        Vector3 position = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 direction = (position - Input.mousePosition).normalized;
+        direction.y = 0.0f;
+
+        m_Rigidbody.AddForce(direction * m_Force);
+    }
+}

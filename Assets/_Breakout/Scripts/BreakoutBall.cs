1e7ff5e75dab488e3e055aaa7212df520ad8bdb5
Renan <renan_ota@hotmail.com>
2019-08-21 22:21:56 -0300

    Aula 02

---
+++ Assets/_Breakout/Scripts/BreakoutBall.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BreakoutBall : MonoBehaviour
+{
+    public float m_Force = 1000.0f;
+    private Rigidbody m_Rigidbody;
+
+    private void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+
+        Vector3 direction = Vector3.forward;
+        direction.x = Random.Range(-1.0f, 1.0f);
+        m_Rigidbody.AddForce(direction * m_Force);
+    }
+}
---
+++ Assets/_Breakout/Scripts/Brick.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    public int m_MaxLife = 5;
+    public Color m_MinColor = Color.blue;
+    public Color m_MaxColor = Color.red;
+
+    private int m_Life;
+    private Renderer m_Renderer;
+
+    private void Start()
+    {
+        m_Renderer = GetComponent<Renderer>();
+        m_Life = Random.Range(1, m_MaxLife + 1);
+        ChangeColor();
+    }
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        m_Life--;
+        if (m_Life < 1) Destroy(gameObject);
+        ChangeColor();
+    }
+
+    private void ChangeColor()
+    {
+        Color color = Color.Lerp(m_MinColor, m_MaxColor, (m_Life / (float)m_MaxLife));
+        m_Renderer.material.SetColor("_Color", color);
+    }
+
+}

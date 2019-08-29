3077479298b77cc19c991182bf9b45342327da0c
Renan <renan_ota@hotmail.com>
2019-08-28 22:25:48 -0300

    Aula 04

---
+++ Assets/_Joints/Scripts/Bridge.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bridge : MonoBehaviour
+{
+    private HingeJoint m_Joint;
+    public float[] m_Targets;
+    private int m_Index = 0;
+
+    private void Awake()
+    {
+        m_Joint = GetComponent<HingeJoint>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            JointSpring spring = m_Joint.spring;
+            
+            m_Index = ++m_Index % m_Targets.Length;
+            spring.targetPosition = m_Targets[m_Index];
+
+            m_Joint.spring = spring;
+        }
+    }
+}
---
+++ Assets/_Joints/Scripts/Wave.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour
+{
+    public float m_Amplitude;
+    public float m_SmoothTime;
+    private Vector3 m_Offset;
+
+    private void Start()
+    {
+        m_Offset = transform.position;
+    }
+
+    private void Update()
+    {
+        float y = m_Amplitude * Mathf.Sin(Time.time * m_SmoothTime);
+        // O Resultado de um Seno sempre vai de -1 a 1.
+        transform.localPosition = m_Offset + Vector3.up * y;
+    }
+}

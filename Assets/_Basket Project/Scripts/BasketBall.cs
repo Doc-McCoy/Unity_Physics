93f9f79ac41ecf71d8060792840aef625ac9cc37
Renan <renan_ota@hotmail.com>
2019-09-04 22:19:01 -0300

    Aula Basquete

---
+++ Assets/_Basket Project/Scripts/BasketBall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketBall : MonoBehaviour
+{
+    public Rigidbody m_BallPrefab;
+    public float m_Force;
+    public Camera m_Camera;
+
+
+    private void Update()
+    {   
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Rigidbody ball = Instantiate<Rigidbody>(m_BallPrefab);
+            ball.transform.position = transform.position;
+            // Direção que a câmera ta olhando * Vetor 'pra frente' * força
+            ball.AddForce(m_Camera.transform.rotation * Vector3.forward * m_Force, ForceMode.VelocityChange);
+        }
+
+    }
+
+}
---
+++ Assets/_Basket Project/Scripts/CameraRotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour
+{
+    public float m_Speed;
+    public Camera m_Camera;
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * m_Speed;
+        float mouseY = Input.GetAxis("Mouse Y") * m_Speed;
+
+        transform.localRotation *= Quaternion.Euler(0, mouseX, 0);
+        m_Camera.transform.localRotation *= Quaternion.Euler(-mouseY, 0, 0);
+
+    }
+
+}

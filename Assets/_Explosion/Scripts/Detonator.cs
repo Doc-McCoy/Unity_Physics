d172f279968c628c40daa2a95fdc0d7af0d0261b
Renan <renan_ota@hotmail.com>
2019-09-04 21:02:28 -0300

    Aula sobre explosões

---
+++ Assets/_Explosion/Scripts/Detonator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detonator : MonoBehaviour
+{
+    public Explosion m_ExplosionScript;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            m_ExplosionScript.Explode();
+            CameraShake.Instance.ShakeOnce(1.0f, 30.0f);
+            Destroy(gameObject);
+        }
+    }
+}
